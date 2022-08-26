9656eec4012a149cf21217295c7c3cd483d0c16b
George Malepeng <jaquar93@gmail.com>
2022-08-26 10:43:36 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-list_names = dir(hidden_4)
-for name in list_names:
-    if name [:2] != "__":
-        print(name)
+    list_names = dir(hidden_4)
+    for name in list_names:
+        if name [:2] != "__":
+            print(name)

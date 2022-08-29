c35b780f3ea0478dcbdeb66f1ecc8b61000364b2
George Malepeng <jaquar93@gmail.com>
2022-08-29 12:13:08 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env/python3
+#!/usr/bin/python3
 def no_c(my_string):
-     new_string = my_string.translate({ord(i): None for i in 'cC'})
-     return new_string
+    return ("".join(c for c in my_string if c not in "Cc"))

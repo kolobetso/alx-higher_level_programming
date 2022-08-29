6a28dab5b8d17b26d1f3852722c51070217ee1a7
George Malepeng <jaquar93@gmail.com>
2022-08-29 13:30:05 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max_num = my_list[0]
+        for a in my_list:
+            if a > max_num:
+                max_num = a
+                return max_num
+            else:
+                return None

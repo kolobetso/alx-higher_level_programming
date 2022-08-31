de0ffdc0b7e6fbb66aaac9acc18454dbd31a4820
George Malepeng <jaquar93@gmail.com>
2022-08-31 15:43:47 -0700

    Task 100 of bonus

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,8 +5,8 @@ def weight_average(my_list=[]):
         frequency = 0
         for tupl in my_list:
             (weight, occurence) = tupl
-            total += (weight * occurence)
+            tot += (weight * occurence)
             frequency += occurence
-        return (total/frequency) if frequency > 0 else 0
+        return (tot/frequency) if frequency > 0 else 0
     else:
         return (0)

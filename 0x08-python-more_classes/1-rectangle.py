c7df4cb4be5008b7828f738b2819f88f5ca17c68
kolobetso <53598770+kolobetso@users.noreply.github.com>
2022-09-20 22:23:43 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,93 +5,7 @@
 class Rectangle:
     """Represent a rectangle."""
 
-    def __init__(self, width=0, height=0):#!/usr/bin/python3
-2
-"""Defines a Rectangle class."""
-3
-​
-4
-class Rectangle:
-5
-    """Rectangle representation."""
-6
-​
-7
-     def __init__(self, width=0, height=0):
-8
-​
-9
-         """instantiate a new Rec
-10
-​
-11
-         Args:
-12
-          width (int): The width of the new rectangle.
-13
-          height (int): The height of the new rectangle.
-14
-​
-15
-        """
-16
-        self.width = width
-17
-        self.height = height
-18
-​
-19
-        @property
-20
-        def width(self):
-21
-             """Get/set the width of the rectangle."""
-22
-             return self.__width
-23
-​
-24
-        @width.setter
-25
-        def width(self, value):
-26
-            if not isinstance(value, int):
-27
-                raise TypeError("width must be an integer")
-28
-            if value < 0:
-29
-                 raise ValueError("width must be >= 0")
-30
-             self.__width = value
-31
-​
-32
-        @property
-33
-        def height(self):
-34
-            """Get/set the height of the rectangle."""
-35
-            return self.__height
-36
-​
-37
-        @height.setter
-38
-        def height(self, value):
-39
-            if not isinstance(value, int):
-40
-                raise TypeError("height must be an integer")
-41
-            if value < 0:
-42
-                raise ValueError("height must be >= 0")
-43
-            self.__height = value
-44
-
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
@@ -125,4 +39,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
